b67f42b4cf6136c7bb57059d800f0b0a102506e2
Anup Sukumaran <anup.sukumaran9@gmail.com>
2018-10-10 22:13:29 +0530

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Sukumar Anup Sukumaran on 10/10/18.
+//  Copyright © 2018 TechTonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
