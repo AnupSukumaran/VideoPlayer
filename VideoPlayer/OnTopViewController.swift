8c870cfb7797954454d68c29cc3c765d7139c478
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2018-10-11 17:02:52 +0530

    new changes

---
+++ VideoPlayer/OnTopViewController.swift
@@ -0,0 +1,31 @@
+//
+//  OnTopViewController.swift
+//  VideoPlayer
+//
+//  Created by Abraham VG on 11/10/18.
+//  Copyright © 2018 TechTonic. All rights reserved.
+//
+
+import UIKit
+
+class OnTopViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- VideoPlayer/ViewController.swift
+++ VideoPlayer/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        playerItem.addObserver(self, forKeyPath: "playbackBufferEmpty", options: .New, context: nil)
+//        playerItem.addObserver(self, forKeyPath: "playbackLikelyToKeepUp", options: .New, context: nil)
+//        playerItem.addObserver(self, forKeyPath: "playbackBufferFull", options: .New, context: nil)
+        
         NotificationCenter.default.addObserver(self, selector:#selector(self.playerDidFinishPlaying(note:)),name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
         
     }
@@ -27,15 +31,39 @@ class ViewController: UIViewController {
     }
     
     @IBAction func playVideo(_ sender: Any) {
-        let video = AVPlayer(url: URL(string: "http://jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v")!)
-        
+        //"http://jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v"
+        let video = AVPlayer(url: URL(string: "https://www.electronicvillage.org/evquizapp/upload/questions/13.mp3")!)
+//
         videoPlayer.player = video
-        videoPlayer.showsPlaybackControls = false
+       // videoPlayer.showsPlaybackControls = false
+
         present(videoPlayer, animated: true) {
-            video.play()
+        print("Playing...")
+           
             
+        let popOverVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "OnTopViewController") as! OnTopViewController
+            self.videoPlayer.addChild(popOverVC)
+            self.videoPlayer.view.addSubview(popOverVC.view)
+            popOverVC.view.center = CGPoint(x: self.videoPlayer.view.bounds.midX, y: self.videoPlayer.view.bounds.midY)
+            popOverVC.didMove(toParent: self.videoPlayer)
+            
+//            let imageView = UIImageView(frame: CGRect(x: popOverVC.view.bounds.midX
+//                , y: popOverVC.view.bounds.midY, width: 50 , height: 50))
+//            imageView.image = UIImage(named: "speaker")
+//            imageView.contentMode = .scaleAspectFit
+//            popOverVC.view.addSubview(imageView)
+        video.play()
+        video.accessibilityElementsHidden = true
+
         }
         
+//        let videoURL = URL(string: "https://www.electronicvillage.org/evquizapp/upload/questions/12.mp4")
+//        let player = AVPlayer(url: videoURL!)
+//        let playerLayer = AVPlayerLayer(player: player)
+//        playerLayer.frame = self.view.bounds
+//        self.view.layer.addSublayer(playerLayer)
+//        player.play()
+//
     }
     
 }
